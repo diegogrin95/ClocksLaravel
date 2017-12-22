e3ba5a34e4c4206d9bb8610c3e8a00f23b0ced21
Ines Casserly <ines.casserly@gmail.com>
2017-12-21 21:19:28 -0300

    Index CSS

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -1,15 +1,19 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-        <link rel="stylesheet" href="/css/app.css">
-    </head>
+@extends ('layouts.app')
+
+@section ('content')
+
+    <!-- <head> -->
+        <!-- <meta charset="utf-8"> -->
+        <!-- <title></title> -->
+        <!-- <link rel="stylesheet" href="/css/app.css"> -->
+    <!-- </head> -->
     <body>
     <ul>
         @foreach ($products as $product)
             <li><a href="/productos/{{$product->id}}">{{$product->name}}</a></li>
         @endforeach
     </ul>
-    </body>
-</html>
+    <!-- </body> -->
+<!-- </html> -->
+
+@endsection
