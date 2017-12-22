b61ab73c05e8743df32c698c7f2b9aebc698b7c4
Diego <diegogrin4@gmail.com>
2017-12-21 21:43:07 -0300

    last

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,45 @@
 <body id="body">
   <main>
     <!-- Banner principal -->
-    <div class="main-banner">Imagen principal</div>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="images/image_1.png" alt="clock1">
+        </div>
+
+        <div class="item">
+          <img class="imgCarrousel" src="images/reloj-frio.jpg" alt="frio">
+        </div>
+
+        <div class="item">
+          <img src="images/reloj-clasico.jpg" alt="clasico">
+        </div>
+
+        <div class="item">
+          <img src="images/reloj-fancy.jpg" alt="fancy">
+        </div>
+
+      </div>
+
+      <!-- Left and right controls -->
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="sr-only">Anterior</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="sr-only">Siguiente</span>
+      </a>
+
+    </div>
+
     <!-- Categorías -->
     <div class="categories-text">
       <h2>¿CUÁL ES TU ESTILO?</h2>
